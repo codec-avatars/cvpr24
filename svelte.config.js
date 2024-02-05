598d0069f3cee27ac6d8d26e0f92cba8e3149223
julietamartinez <julietamartinez@meta.com>
2024-02-05 14:46:59 -0500

    output build to github-pages

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,8 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-            fallback: 'index.html' // may differ from host to host
+            fallback: 'index.html', // may differ from host to host
+			pages: "github-pages"
         })
 	},
 
