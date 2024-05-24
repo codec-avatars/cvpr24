c7dadb12a9b6f2b4017560af08a5f7621e798f59
Julieta Martinez <jltmtzc@gmail.com>
2024-05-24 14:46:44 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -70,10 +70,10 @@
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
         
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-			Workshop on Codec Avatars @ CVPR24
+			Social Presence with Codec Avatars
 		</h1>
         <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-			Full-day workhsop on Tuesday, June 18, 2024, at the Seattle Convention Center.
+			Full-day CVPR'24 workshop on Tuesday, June 18, 2024, at the Seattle Convention Center.
 		</p>
         
     </div>
