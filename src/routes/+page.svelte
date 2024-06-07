89605d718355d4ad4bbdeb25a691a9e8557abcd3
julietamartinez <julietamartinez@meta.com>
2024-06-07 12:41:42 -0400

    update start time

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -116,7 +116,7 @@
 				<tbody>
 					<tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
 						<th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-							9:00 - 10:15
+							9:30 - 10:15
 						</th>
 						<td class="px-6 py-4">
 							TBD
